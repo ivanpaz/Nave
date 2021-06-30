f5e8f74515ae44ec335498bbced54dda622bd79e
ivanpaz <ipaz@kokku.com.br>
2021-06-30 20:17:35 -0300

    Primeiro commit

---
+++ Assets/Scripts/PersonalCamera.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersonalCamera : MonoBehaviour
+{
+
+    [SerializeField]
+    Transform target ;
+
+    [SerializeField]
+    Transform camPoint;
+
+    float distance = 10f;
+    float rotSpeed = 5f;
+
+    Vector3 camPosition;
+
+    Vector3 smoothTransition;
+
+    float smoothTime = 1f;
+    float angle;
+
+       
+       
+    void FixedUpdate()
+    {
+        CameraFinal();
+    }
+
+    void CameraFixa()
+    {
+        transform.position = camPoint.position;
+        transform.rotation = camPoint.rotation;
+    }
+
+    void CameraTop()
+    {
+        transform.position = target.position + target.up * distance + target.forward * (distance * 0.3f);
+        transform.rotation = target.rotation * Quaternion.Euler(90f, 0, 0);
+    }
+
+    void CameraFinal()
+    {
+        camPosition = target.position - (target.forward * distance) + target.up * distance /3;
+        smoothTransition = Vector3.Lerp(transform.position, camPosition, smoothTime);
+        transform.position = smoothTransition;
+
+        angle = Mathf.Abs(Quaternion.Angle(transform.rotation, target.rotation));
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, target.rotation, (rotSpeed + angle ) * Time.deltaTime);
+    }
+
+
+}
---
+++ Assets/Scripts/ShipController.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipController : MonoBehaviour
+{
+    CharacterController controller;
+
+    float variant = 10f;
+    float speed = 10f;
+    float rotSpeedX = 6f;
+    float rotSpeedY = 3f;
+
+    float roll = 0;
+
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+  
+    private void FixedUpdate()
+    {
+        Movement();
+    }
+
+    void Movement()
+    {
+        Vector3 moveDir = transform.forward * speed * variant;
+
+        Vector3 yaw = Input.GetAxisRaw("Horizontal") * transform.right * rotSpeedX * Time.deltaTime * variant;
+        Debug.Log("Yaw - " + yaw);
+        Vector3 pitch = Input.GetAxisRaw("Vertical") * transform.up * rotSpeedY * Time.deltaTime * variant;
+        Debug.Log("Pitch - " + pitch);
+        Vector3 dir = yaw + pitch;
+
+        float maxX = Quaternion.LookRotation(moveDir + dir).eulerAngles.x;
+
+        if (maxX < 90 && maxX > 60 || maxX >260 && maxX <290)
+        {
+
+        }
+        else
+        {
+            moveDir += dir;
+            transform.rotation = Quaternion.LookRotation(moveDir);
+        }
+        GetRoll(Input.GetAxisRaw("Horizontal"));
+        Debug.Log(roll);
+        transform.Rotate(0, 0, roll);
+        controller.Move(moveDir * Time.deltaTime);
+    }
+
+    void GetRoll(float axis)
+    {
+        
+        //Voltar para a rotação inicial
+        if (axis == 0)
+        {
+            if (roll < 0)
+            {
+                roll += 1;
+            }
+            else if(roll > 0)
+            {
+                roll -= 1;
+            }
+        //Rotacionar para a direita
+        }else if(axis > 0)
+        {
+            if (roll > -30)
+            {
+                roll -= axis;
+            }
+        }
+        //Rotacionar para a esquerda
+        else
+        {
+            if (roll < 30)
+            {
+                roll -= axis;
+            }
+
+        }
+
+    }
+}
